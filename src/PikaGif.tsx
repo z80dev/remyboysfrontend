32e45466eced80194da621598ec93291cb1a072e
z80 <z80@ophy.xyz>
2024-04-09 15:19:58 -0400

    add gif file

---
+++ src/PikaGif.tsx
@@ -0,0 +1,11 @@
+const PikaGif = () => {
+  const gifUrl = 'https://i.redd.it/q0dd3k02unqb1.gif';
+
+  return (
+    <div className="retro-gif">
+      <img src={gifUrl} alt="Retro GIF" />
+    </div>
+  );
+};
+
+export default PikaGif;
