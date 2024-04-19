79e1d5ff3adfef7e3d7083e80392dbb1f46e64c0
z80 <z80@ophy.xyz>
2024-04-18 23:40:03 -0400

    add mint ability

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,112 @@
 // @ts-nocheck
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import { useSwitchChain, useChainId } from 'wagmi'
+import { useSwitchChain, useChainId, useWriteContract } from 'wagmi'
 import { useReadContract } from 'wagmi'
 import { whitelists } from './whitelists.ts'
 import PikaGif from './PikaGif.tsx'
+import { useState } from 'react';
+import addresses from './addresses.ts'
+import { formatEther } from 'viem'
+
+function MintFieldset({ account }) {
+
+  const [mintAmount, setMintAmount] = useState(1);
+  const { writeContract } = useWriteContract()
+
+  const collectionName = useReadContract({
+    abi: NFTAbi,
+    address: addresses.NFT,
+    functionName: 'name',
+    args: [],
+  })
+
+  const collectionTotalSupply = useReadContract({
+    abi: NFTAbi,
+    address: addresses.NFT,
+    functionName: 'totalSupply',
+    args: [],
+  })
+
+  const mintCost = useReadContract({
+    abi: VendorAbi,
+    address: addresses.Vendor,
+    functionName: 'quote_mint',
+    args: [mintAmount],
+    account: account,
+  })
+
+  const handleIncrement = () => {
+    setMintAmount((prevAmount) => prevAmount + 1);
+  };
+
+  const handleDecrement = () => {
+    if (mintAmount > 1) {
+      setMintAmount((prevAmount) => prevAmount - 1);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    if (!isNaN(value) && value >= 1) {
+      setMintAmount(value);
+    }
+  };
+
+  const handleMintClick = () => {
+    console.log('minting', mintAmount, 'NFTs')
+    writeContract({
+      abi: VendorAbi,
+      address: addresses.Vendor,
+      functionName: 'mint_nft',
+      args: [mintAmount],
+      account: account,
+      value: mintCost.data
+    })
+  }
+
+  return (
+    <fieldset>
+      <legend>Mint {collectionName.data}</legend>
+      <div className="mint-progress">
+        <div role="progressbar" aria-valuenow={collectionTotalSupply.data?.toString() ?? "0"} aria-valuemin="0" aria-valuemax="4444">
+      <div style={{ width: `${(collectionTotalSupply.data.toString() / 4444) * 100}%` }} />
+        </div>
+        <p>Mint Progress: {collectionTotalSupply.data?.toString() ?? 0} / 4444</p>
+      </div>
+      <div className="mint-fieldset">
+        <div className="mint-amount">
+          <button className="modbutton" type="button" onClick={handleDecrement}>
+            -
+          </button>
+          <input
+            type="text"
+            className="mint-input"
+            value={mintAmount}
+            onChange={handleInputChange}
+          />
+          <button type="button" className="modbutton" onClick={handleIncrement}>
+            +
+          </button>
+        </div>
+        <p>Cost: {formatEther(mintCost.data ?? 0)} ETH</p>
+        <button type="button" onClick={() => {
+          console.log('minting', mintAmount, 'NFTs')
+          console.log('mintCost', mintCost.data)
+          console.log('account', account)
+          console.log('abi', VendorAbi)
+          console.log('address', addresses.Vendor)
+          writeContract({
+            abi: VendorAbi,
+            address: addresses.Vendor,
+            functionName: 'mint_nft',
+            args: [mintAmount],
+            value: mintCost.data
+          })
+        }}>Mint</button>
+      </div>
+    </fieldset>
+  )
+}
 
 function AccountDetails({ account, disconnect }) {
   return (
@@ -11,7 +114,6 @@ function AccountDetails({ account, disconnect }) {
       <legend>Account Details</legend>
       <div className="account-details">
         <p>Address: {account.address}</p>
-        <p>Chain ID: {account.chainId}</p>
         <button type="button" onClick={() => disconnect()}>
           Disconnect
         </button>
@@ -63,6 +165,64 @@ function StatusBar({ status, error }: { status: any, error: any }) {
   )
 }
 
+const VendorAbi = [
+  // { "stateMutability": "nonpayable", "type": "constructor", "inputs": [{ "name": "_whitelist_manager", "type": "address" }, { "name": "_nft", "type": "address" }], "outputs": [] },
+  // { "stateMutability": "nonpayable", "type": "function", "name": "open_mint", "inputs": [], "outputs": [] },
+  // { "stateMutability": "nonpayable", "type": "function", "name": "close_mint", "inputs": [], "outputs": [] },
+  { "stateMutability": "view", "type": "function", "name": "quote_mint", "inputs": [{ "name": "amt", "type": "uint256" }], "outputs": [{ "name": "", "type": "uint256" }] },
+  { "stateMutability": "payable", "type": "function", "name": "mint_nft", "inputs": [{ "name": "amt", "type": "uint256" }], "outputs": [] },
+  // { "stateMutability": "view", "type": "function", "name": "mint_open", "inputs": [], "outputs": [{ "name": "", "type": "bool" }] }
+]
+
+const NFTAbi = [
+  {
+    "inputs": [],
+    "stateMutability": "view",
+    "type": "function",
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ]
+  },
+  {
+    "inputs": [],
+    "stateMutability": "view",
+    "type": "function",
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ]
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ]
+  },
+]
+
+
 function App() {
   const { chains, switchChain } = useSwitchChain()
   const account = useAccount()
@@ -71,7 +231,7 @@ function App() {
   const { disconnect } = useDisconnect()
   const result = useReadContract({
     abi: [{ stateMutability: "payable", type: "constructor", inputs: [], outputs: [] }, { stateMutability: "view", type: "function", name: "check_whitelist", inputs: [{ name: "i", type: "uint256" }, { name: "addr", type: "address" }], outputs: [{ name: "", type: "uint256" }] }, { stateMutability: "view", type: "function", name: "check_whitelists", inputs: [{ name: "addr", type: "address" }], outputs: [{ name: "", type: "uint256[]" }] }, { stateMutability: "nonpayable", type: "function", name: "set_manager", inputs: [{ name: "new_manager", type: "address" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "set_spender", inputs: [{ name: "new_spender", type: "address" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "add_whitelist", inputs: [{ name: "typ", type: "uint256" }, { name: "limit", type: "uint256" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "set_whitelist_limit", inputs: [{ name: "i", type: "uint256" }, { name: "limit", type: "uint256" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "set_whitelist_limit_for_address", inputs: [{ name: "whitelist_id", type: "uint256" }, { name: "addr", type: "address" }, { name: "limit", type: "uint256" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "whitelist_addresses", inputs: [{ name: "i", type: "uint256" }, { name: "addrs", type: "address[]" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "whitelist_addresses", inputs: [{ name: "i", type: "uint256" }, { name: "addrs", type: "address[]" }, { name: "limit", type: "uint256" }], outputs: [] }, { stateMutability: "nonpayable", type: "function", name: "spend_whitelists", inputs: [{ name: "addr", type: "address" }, { name: "amounts", type: "uint256[]" }], outputs: [] }, { stateMutability: "view", type: "function", name: "manager", inputs: [], outputs: [{ name: "", type: "address" }] }, { stateMutability: "view", type: "function", name: "spender", inputs: [], outputs: [{ name: "", type: "address" }] }, { stateMutability: "view", type: "function", name: "whitelists", inputs: [{ name: "arg0", type: "uint256" }], outputs: [{ name: "", type: "tuple", components: [{ name: "typ", type: "uint256" }, { name: "limit", type: "uint256" }] }] }],
-    address: '0x964287f5ED2B6b99eaE83bf190ac9B735347Ada2',
+    address: addresses.Whitelist,
     functionName: 'check_whitelists',
     args: [account.address ?? "0x0000"],
   })
@@ -89,6 +249,7 @@ function App() {
         </div>
         <div className="window-body">
           <PikaGif />
+          <MintFieldset account={account} />
           {account.status === 'connected' && (
             <div className="connected-content">
               <AccountDetails account={account} disconnect={disconnect} />
---
+++ src/addresses.ts
@@ -0,0 +1,7 @@
+const addresses = {
+    NFT: '0x961c8A568bB5A9A07F7500a9B85fb5A539a70e26',
+    Vendor: '0xcaA624c4B4E0117040DF86e760CC4d5e2bAf86a1',
+    Whitelist: '0xA7a20Ec48114Fa7C31E559B48277eE8B523f2081'
+} as const
+
+export default addresses
